9c60b59316233a4fbdb926e4b2ea383a651f2d1c
Dariia <dariikhom@gmail.com>
2025-12-08 21:25:35 +0200

    auth modals changes

--- frontend/app/(dashboard)/account/page.tsx
+++ frontend/app/(dashboard)/account/page.tsx
@@ -1,10 +1,56 @@
+"use client";
 import styles from "./page.module.css";
-import UserAva from "../../../public/icons/user-avatar.svg";
-
+import { useState, useRef } from "react";
+import EditProfile from "../../../src/components/Authorization/Edit-profile"
+import ChangePasswordModal from "@/src/components/Authorization/Change-password";
 export default function Account() {
+
+    const testuser = {
+        username: "birde",
+        firstName: "Alex",
+        lastName: "Shoqho",
+        email: "user@email.com",
+        country: "Ukraine",
+        birthdate: "Dec 14, 1996",
+        age: "28",
+        password: "12345678",
+        ava: ""
+
+    }
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [isPasswordEditing, setIsPasswordEditing] = useState(false);
+
+
+    const [username, setUsername] = useState(testuser.username);
+    const [firstName, setFirstName] = useState(testuser.firstName);
+    const [lastName, setLastName] = useState(testuser.lastName);
+    const [email, setEmail] = useState(testuser.email);
+    const [country, setCountry] = useState(testuser.country);
+    const [birthdate, setBirthdate] = useState(testuser.birthdate);
+    const [age, setAge] = useState(testuser.age);
+    const [password, setPassword] = useState(testuser.password);
+    const [userAva, setUserAva] = useState(testuser.ava);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+
+
+
+    const handleButtonClick = () => {
+        fileInputRef.current?.click();
+    };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        const imageUrl = URL.createObjectURL(file);
+        setUserAva(imageUrl);
+    }
+
+
     return (
         <div className={styles["account-page"]}>
-            <h1 className={styles["account-title"]}>My account</h1>
+            <h1 className={styles["account-title"]}>Your Profile</h1>
 
             <div className={styles["btn-container"]}>
                 <button className={styles["btn"]}>Favorites</button>
@@ -13,52 +59,124 @@ export default function Account() {
 
             <div className={styles["account-container"]}>
                 <h2 className={styles["greeting"]}>Welcome, <b>birdie!</b></h2>
-                
-                <div className={styles["user-info"]}> 
-                    <div className={styles["text-info"]}>
-                        <div className={styles["names"]}>
-                            <div>
-                                <p>Username</p>
-                                <input value="birde" readOnly></input>
+
+                <div className={styles["user-info"]}>
+                    <div>
+                        <div className={styles["text-info"]}>
+                            <div className={styles["names"]}>
+                                <div>
+                                    <p>Username</p>
+                                    <input value={username} readOnly={!isEditing} onChange={e => setUsername(e.target.value)} ></input>
+                                </div>
+                                <div>
+                                    <p>First Name</p>
+                                    <input value={firstName} readOnly={!isEditing} onChange={e => setFirstName(e.target.value)} ></input>
+                                </div>
+                                <div>
+                                    <p>Last Name</p>
+                                    <input value={lastName} readOnly={!isEditing} onChange={e => setLastName(e.target.value)} ></input>
+                                </div>
+
                             </div>
+                            <div><div>
+                                <p>Email</p>
+                                <input value={email} className={styles["email"]} readOnly={!isEditing} onChange={e => setEmail(e.target.value)} ></input>
+                            </div></div>
                             <div>
-                                <p>First Name</p>
-                                <input value="Alex" readOnly></input>
+                                <div>
+                                    <p>Country</p>
+                                    <input value={country} readOnly={!isEditing} onChange={e => setCountry(e.target.value)} ></input>
+                                </div>
+                                <div>
+                                    <p>Date of Birth</p>
+                                    <input value={birthdate} readOnly={!isEditing} onChange={e => setBirthdate(e.target.value)} ></input>
+                                </div>
+                                <div>
+                                    <p>Age</p>
+                                    <input value={age} readOnly={!isEditing} onChange={e => setAge(e.target.value)} ></input>
+                                </div>
+
+
                             </div>
+                            {isEditing && (
+                                <div className={styles["password-change-container"]}>
+                                    <div>
+                                        <p className={styles["password-change-title"]}>Password</p>
+                                        <input
+                                            type="password"
+                                            value={password}
+                                            readOnly
+                                            placeholder="Enter new password"
+                                        />
+                                        <button
+                                            className={styles["edit-password-btn"]}
+                                            onClick={() => setIsPasswordEditing(!isPasswordEditing)}
+                                        >
+                                            Change password
+                                        </button>
+
+                                        <ChangePasswordModal
+                                            isOpen={isPasswordEditing}
+                                            onClose={() => setIsPasswordEditing(false)}
+                                            oldPassword={testuser.password}
+                                        />
+                                    </div>
+                                </div>
+                            )}
                             <div>
-                                <p>Last Name</p>
-                                <input value="Shoqho" readOnly></input>
+                                <label className={styles["checkbox-wrapper"]}>
+                                    <input type="checkbox" className={styles["checkbox"]} />
+                                    <span className={styles["custom-checkbox"]}></span>
+                                    <span className={styles["checkbox-text"]}>Email Notifications</span>
+                                </label>
                             </div>
-                            
                         </div>
-                        <div><div>
-                            <p>Email</p>
-                            <input value="user@email.com" id="email-input" readOnly></input>
-                        </div></div>
-                        <div>
-                            <div>
-                                <p>Country</p>
-                                <input value="Ukraine" readOnly></input>
-                            </div>
-                            <div>
-                                <p>Date of Birth</p>
-                                <input value="Dec 14, 1996 " readOnly></input>
-                            </div>
-                            <div>
-                                <p>Age</p>
-                                <input value="28" readOnly></input>
-                            </div>
-                            
+                        <div className={styles["user-ava"]}>
+                            <img src={userAva == "" ? "/icons/user-avatar.svg" : userAva} alt="avatar" className="d" />
+                            {isEditing && (
+                                <button
+                                    className={styles["edit-photo-btn"]}
+                                    onClick={handleButtonClick}
+                                >
+                                    <input
+                                        type="file"
+                                        ref={fileInputRef}
+                                        style={{ display: "none" }}
+                                        accept="image/*"
+                                        onChange={handleImageChange}
+                                    />
+                                    Change photo
+                                </button>
+                            )}
+
                         </div>
+
                     </div>
-                    <div className={styles["user-ava"]}>
-                        <img src="/icons/user-avatar.svg" alt="avatar" className={styles["user-ava"]} />
-                    </div>
+
+
+
                 </div>
+
+                <div className={styles["edit-profile-btn-container"]}>
+                    <button
+                        className={styles["edit-profile-btn"]}
+                        onClick={() => setIsEditing(!isEditing)}
+                    >
+                        {isEditing ? "Save" : "Edit profile"}
+                    </button>
+                    <button
+                        className={styles["edit-profile-btn"]}
+                    /*onClick=handleLogOut*/
+                    >
+                        Exit
+                    </button>
+                </div>
+
+
 
             </div>
         </div>
     );
-    
+
 }
 
--- frontend/src/components/Authorization/Reset-password.tsx
+++ frontend/src/components/Authorization/Reset-password.tsx
@@ -1,6 +1,47 @@
-import styles from "./Auth.module.css";
+import { useState } from "react";
+import styles from "./Change-password.module.css";
 
 
-export default function ResetPassword() {
-    return <h1>Reset Password</h1>
+interface ResetPassword {
+    onClose: () => void;
+}
+
+export default function ResetPassword({
+    onClose }: ResetPassword) {
+
+    const [email, setEmail] = useState("");
+
+    const sendResetLink = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        /*sending reset link*/
+        onClose();
+    };
+
+
+    return (
+        <div className={styles["overlay"]}>
+            <div className={styles["modal"]}>
+                <button className={styles["close-btn"]} onClick={onClose}>
+                    <img src="/icons/close.svg" alt="close-btn"></img>
+                </button>
+
+                <h2>Forgot your password?</h2>
+                <div className={styles["field"]}>
+                    <p className={styles["descr"]}>Don’t worry! Enter your email address below, and we’ll send you a link to reset your password.</p>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        placeholder="enter your registered email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <button onClick={sendResetLink} className={styles["save-btn"]}>
+                    Send Reset Link
+                </button>
+            </div>
+
+        </div>
+    );
+
 }
